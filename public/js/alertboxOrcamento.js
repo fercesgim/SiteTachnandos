22333f6259627362dd0f2579d1313c5473b65100
fercesgim <fernandocesgim@gmail.com>
2024-12-17 23:06:12 -0300

    feat: Coreeção da menagem de sucesso do orçamento

--- public/js/alertboxOrcamento.js
+++ public/js/alertboxOrcamento.js
@@ -1,15 +1,20 @@
  // Seleciona o formulário e a seção de mensagem
- const formulario = document.getElementById('user_contact');
+ const formulario = document.getElementById('teste');
  const mensagem = document.getElementById('mensagem');
  const fecharMensagem = document.querySelector('.fechar-mensagem');
-
  // Adiciona um evento de submit ao formulário
  formulario.addEventListener('submit', function(event) {
      event.preventDefault(); // Impede o envio padrão do formulário
      mensagem.classList.remove('d-none'); // Mostra a mensagem
+     //Fecha a mensagem automaticamente após 5 segundos
+     setTimeout(() => {
+        mensagem.classList.add('d-none')
+      }, 5000);
  });
-
  // Adiciona um evento para fechar a mensagem ao clicar no ícone
  fecharMensagem.addEventListener('click', function() {
      mensagem.classList.add('d-none'); // Esconde a mensagem novamente
  });
+
+
+ 
